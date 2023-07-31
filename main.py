0fd2ad4bb6ba7520fb85982ebc8bc7695931d7c7
elilcatness <elilcatness@gmail.com>
2023-08-01 00:25:37 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+from csv import DictReader, DictWriter
+from dotenv import load_dotenv
+
+from alive_progress import alive_bar
+
+from src.constants import DELIMITER
+
+
+def main():
+    output_filename = os.getenv('output_filename', 'output.csv')
+    initial_data = []
+    for key, default in [('volume_filename', 'volume.csv'),
+                         ('difficulty_filename', 'difficulty.csv'),
+                         ('score_filename', 'score.csv')]:
+        filename = os.getenv(key, default)
+        try:
+            with open(filename, encoding='utf-8') as f:
+                first_line = f.readline().strip()
+                empty_msg = f'Файл {filename} пуст!'
+                if not first_line or not (rows := list(DictReader(
+                        f, first_line.split(DELIMITER), delimiter=DELIMITER))):
+                    return print(empty_msg)
+                initial_data.append(rows)
+        except FileNotFoundError:
+            return print(f'Файл {filename} не найден!')
+    delete_empty = input('Удалять «пустые» значения? (y\\n): ').lower() == 'y'
+    data_fieldnames = ['Volume', 'Difficulty', 'Score']
+    data = dict()
+    for sub_data, data_key in zip(initial_data, data_fieldnames):
+        with alive_bar(len(sub_data), title=f'Агрегация {data_key}') as bar:
+            for row in sub_data:
+                q = row['Запрос']
+                for country_key in row:
+                    if country_key == 'Запрос':
+                        continue
+                    country = country_key.split('_')[0]
+                    if (key := f'{q}|{country}') not in data:
+                        data[key] = {data_key: row[country_key]}
+                    else:
+                        data[key][data_key] = row[country_key]
+                bar()
+    with open(output_filename, 'w', newline='', encoding='utf-8') as f:
+        writer = DictWriter(f, ['Query|Country'] + data_fieldnames, delimiter=DELIMITER)
+        writer.writeheader()
+        with alive_bar(len(data), title=f'Запись в {output_filename}\t') as bar:
+            for key, val in sorted(data.items(), key=lambda t: float(t[1]['Score']), reverse=True):
+                if delete_empty and any(sub_val for sub_val in val.values()):
+                    writer.writerow({'Query|Country': key, **val})
+                bar()
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    main()
